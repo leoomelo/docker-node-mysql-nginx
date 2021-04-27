2a38f4cd028856abc5bcfa11cb3f0a1d00e9de9a
Leonardo Melo <leonardo_melo@apple.com>
2021-04-27 10:10:34 -0300

    avoid close connection

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 const express = require('express')
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const app = express()
 
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: '172.18.0.2',
   user: 'admin',
   password: 'root',
   database: 'pfa'
 });
 
-connection.connect();
+// connection.connect();
 
 app.get('/', (req, res) => {
   connection.query('SELECT name FROM full_cycle_modules', function (error, results) {
     if (error) throw error;
     res.send(results);
-    connection.end();
+    // connection.end();
   });
 })
 
